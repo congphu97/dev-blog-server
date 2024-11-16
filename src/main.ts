c75aa3b14a052bc6ce58aff5b2671045d5d1f052
congphu97 <39950332+congphu97@users.noreply.github.com>
2024-11-16 23:37:19 +0700

    fix: deploy render.com

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const port = process.env.PORT || 3000; // Use Render's PORT or default to 3000
+  app.enableCors({
+    origin: 'https://dev-blog-server-rgnj.onrender.com', // Allow requests from this origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Optional, depending on your setup
+  });
+
   await app.listen(port);
   console.log(`Application is running on: http://localhost:${port}`);
 }
