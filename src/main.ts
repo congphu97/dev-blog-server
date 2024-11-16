b74b9a0f46ad3ff077974de42136d229ae110c1f
congphu97 <39950332+congphu97@users.noreply.github.com>
2024-11-16 22:00:01 +0700

    fix: deploy render.com

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import * as dotenv from 'dotenv';
-
-// dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const port = process.env.PORT || 3000; // Use Render's PORT or default to 3000
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
+
 bootstrap();
